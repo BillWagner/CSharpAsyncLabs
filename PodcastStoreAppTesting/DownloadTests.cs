d7f18145ea4efbba93a6766ce5ddde8ad295940b
Bill Wagner <bill.wagner@srtsolutions.com>
2013-01-07 13:52:50 -0500

    Final version
    This version contains some initial tests that exercise the different
    code paths through the async portions of the application

---
+++ PodcastStoreAppTesting/DownloadTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using RoseSniffingPodcasts.Data;
+using System.Threading.Tasks;
+using RoseSniffingPodcasts.Services;
+using Windows.Storage;
+
+namespace PodcastStoreAppTesting
+{
+
+    public class EpisodeDownloaderStub : IEpisodeDownloader
+    {
+        private Task task;
+        public void SetReturnValue(Task downloadTask)
+        {
+            this.task = downloadTask;
+        }
+        public Task SaveUrlAsync(Windows.Storage.StorageFolder folder, System.Net.Http.HttpClient client, string path)
+        {
+            return task;
+        }
+        public Task<StorageFolder> VerifyFolderCreation()
+        {
+            return Task.FromResult(default(StorageFolder));
+        }
+    }
+
+    [TestClass]
+    public class DownloadTests
+    {
+        [TestMethod]
+        public async Task TestFastDownload()
+        {
+            int executeChanged = 0;
+            var downloader = new EpisodeDownloaderStub();
+            var task = new Task(() => {});
+            task.Start();
+            downloader.SetReturnValue(task);
+            var underTest = new PodcastSeries("1", "testCollection", string.Empty, "A test collection", downloader);
+            underTest.DownloadHander.CanExecuteChanged +=(_, __) => executeChanged++;
+            PodcastEpisode episode = new PodcastEpisode("2", "title", "subtitle", string.Empty, "http://nowhere.com/episode1.mp3", "This is a podcast", underTest);
+            underTest.Items.Add(episode);
+            underTest.SelectedEpisodes.Add(episode);
+            underTest.DownloadHander.Execute(null);
+
+            // want to await the ownded task.
+            // check the result
+            await underTest.ActiveDownload;
+
+            Assert.AreEqual(2, executeChanged);
+            Assert.AreEqual(0, underTest.SelectedEpisodes.Count);
+        }
+
+        [TestMethod]
+        public async Task TestSlowDownload()
+        {
+            int executeChanged = 0;
+            var downloader = new EpisodeDownloaderStub();
+            var task = new Task(() => { });
+            downloader.SetReturnValue(task);
+            var underTest = new PodcastSeries("1", "testCollection", string.Empty, "A test collection", downloader);
+            underTest.DownloadHander.CanExecuteChanged += (_, __) => executeChanged++;
+            PodcastEpisode episode = new PodcastEpisode("2", "title", "subtitle", string.Empty, "http://nowhere.com/episode1.mp3", "This is a podcast", underTest);
+            underTest.Items.Add(episode);
+            underTest.SelectedEpisodes.Add(episode);
+            underTest.DownloadHander.Execute(null);
+
+            Assert.AreEqual(1, executeChanged);
+            Assert.AreEqual(1, underTest.SelectedEpisodes.Count);
+
+            task.Start();
+
+            // want to await the ownded task.
+            // check the result
+            await underTest.ActiveDownload;
+
+
+            Assert.AreEqual(2, executeChanged);
+            Assert.AreEqual(0, underTest.SelectedEpisodes.Count);
+        }
+
+        [TestMethod]
+        public async Task TestErrorDownload()
+        {
+            int executeChanged = 0;
+            var downloader = new EpisodeDownloaderStub();
+            var task = new Task(() => { throw new InvalidOperationException("Well, that failed"); });
+            downloader.SetReturnValue(task);
+            var underTest = new PodcastSeries("1", "testCollection", string.Empty, "A test collection", downloader);
+            underTest.DownloadHander.CanExecuteChanged += (_, __) => executeChanged++;
+            PodcastEpisode episode = new PodcastEpisode("2", "title", "subtitle", string.Empty, "http://nowhere.com/episode1.mp3", "This is a podcast", underTest);
+            underTest.Items.Add(episode);
+            underTest.SelectedEpisodes.Add(episode);
+            underTest.DownloadHander.Execute(null);
+
+            Assert.AreEqual(1, executeChanged);
+            Assert.AreEqual(1, underTest.SelectedEpisodes.Count);
+
+            task.Start();
+
+            // want to await the ownded task.
+            // check the result
+            try
+            {
+                await underTest.ActiveDownload;
+                Assert.Fail("Exception not generated");
+            }
+            catch (InvalidOperationException)
+            {
+                return;
+            }
+        }
+    
+    }
+}
---
+++ PodcastStoreAppTesting/SeriesDownloaderTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using RoseSniffingPodcasts.Data;
+using RoseSniffingPodcasts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodcastStoreAppTesting
+{
+    public class FeedDownloaderStub : IFeedDownloader
+    {
+        private Task<Series> result;
+
+        public Task<Series> RetrieveFeed(string feedAddr)
+        {
+            return result;
+        }
+
+        // Change this to take an array of 7 tasks. 
+        // Return each task in order.
+        // cycle if the array is comlpeted.
+        public void SetResult(Task<Series> result)
+        {
+            this.result = result;
+        }
+    }
+
+    [TestClass]
+    public class SeriesDownloaderTests
+    {
+        [TestMethod]
+        public void NullDownloaderFails()
+        {
+            var undertest = new PodcastCollection(default(IFeedDownloader));
+            Assert.AreEqual(0, undertest.AllGroups.Count);
+        }
+
+        [TestMethod]
+        public async Task FastDownloadWorks()
+        {
+            var downloader = new FeedDownloaderStub();
+            var series = new Series();
+            await series.SetEpisodes(Task.FromResult(new List<Episode>().AsEnumerable()));
+            downloader.SetResult(Task.FromResult(series));
+            var underTest = new PodcastCollection(downloader);
+
+            Assert.AreEqual(7, underTest.AllGroups.Count);
+        }
+
+        [TestMethod]
+        public async Task SlowDownloadWorks()
+        {
+            var downloader = new FeedDownloaderStub();
+            var series = new Series();
+
+            var episodeTCS = new TaskCompletionSource<IEnumerable<Episode>>();
+            var downloadTCS = new TaskCompletionSource<Series>();
+
+            var parseTask = series.SetEpisodes(episodeTCS.Task);
+            downloader.SetResult(downloadTCS.Task);
+
+            var underTest = new PodcastCollection(downloader);
+
+            episodeTCS.TrySetResult(new List<Episode>().AsEnumerable());
+            downloadTCS.TrySetResult(series);
+            await parseTask;
+
+            Assert.AreEqual(7, underTest.AllGroups.Count);
+        }
+
+        [TestMethod]
+        public async Task ExcpetionsCreateEmptySeries()
+        {
+            var downloader = new FeedDownloaderStub();
+            var series = new Series();
+
+            var episodeTCS = new TaskCompletionSource<IEnumerable<Episode>>();
+            var downloadTCS = new TaskCompletionSource<Series>();
+
+            var parseTask = series.SetEpisodes(episodeTCS.Task);
+            downloader.SetResult(downloadTCS.Task);
+
+            var underTest = new PodcastCollection(downloader);
+
+            episodeTCS.TrySetResult(new List<Episode>().AsEnumerable());
+            downloadTCS.TrySetException(new NullReferenceException());
+            await parseTask;
+
+            Assert.AreEqual(0, underTest.AllGroups.Count);
+        }
+
+
+
+    }
+}
